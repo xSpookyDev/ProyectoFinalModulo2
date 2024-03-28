05bbd295ba2bec70e47b24c36677c414d55f13c7
xSpookyDev <ricardo13dev@gmail.com>
2024-03-27 23:06:18 -0300

    casi terminado

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+//scripte para esconder parrafos en cards
+$(document).ready(function(){
+    $(".img-fluid").click(function(e){
+        e.preventDefault(); 
+        
+        $(this).closest(".card").find(".card-text").toggle();
+    });
+});
+
+//script smooth scroll, aunque me gustaba mas en html
+$(document).ready(function() {
+   
+    $('header nav ul.navbar-nav li a.nav-link').on('click', function(event) {
+      
+      event.preventDefault();
+
+     
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+      
+      if (target.length) {
+      
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000); 
+      }
+    });
+  });
